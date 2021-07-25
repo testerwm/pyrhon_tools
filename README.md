e692c3df4bd812ab04d23cd7af5ef04dff88943e
wangming <tester15267014144@163.com>
2021-07-25 10:19:41 +0800

    微信轰炸脚本提交

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pyrhon_tools
+该项目为python小工具合集：
+1、微信轰炸小工具
---
+++ wechat_auto_sendmessage.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2021/7/24 4:28 下午
+# @Author  : 山西_王二狗
+# @Site    : 
+# @File    : 微信自动发送.py
+# @Software: PyCharm
+
+import time
+from pynput.keyboard import  Key, Controller as key_cl # 键盘的控制器
+from pynput.mouse import Button, Controller as mouse_cl  # 鼠标的控制器
+
+
+# 键盘的控制函数
+
+def keyboard_input(string):
+    keyboard = key_cl() # 获取鼠标的权限
+    keyboard.type(string) # 设置数据的类型
+
+# 鼠标的控制函数
+
+def mouse_click():
+    mouse = mouse_cl() # 获取鼠标的权限
+    mouse.press(Button.left) # 模拟鼠标左键的按下
+    mouse.release(Button.left) # 模拟鼠标左键的弹起
+
+
+# 实现消息的发送
+def send_message(number, string):
+    print("程序在三秒钟之后开始执行")
+    time.sleep(3)
+    keyboard = key_cl()
+
+    for i in range(number):
+        keyboard_input(string)
+        mouse_click()
+        time.sleep(2)
+
+        keyboard.press(Key.enter)# 模拟回车键的按下
+
+        keyboard.release(Key.enter)# 模拟回车键的弹起
+
+if __name__=="__main__":
+    send_message(1314, '你大点声，看不到你的文字！！！！ 你的手速不行')
